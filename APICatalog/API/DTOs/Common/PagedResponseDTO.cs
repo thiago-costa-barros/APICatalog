ca32f94093af3f3b4265ab9420a1ff8e1e0b49ea
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-05-11 13:55:51 -0300

    feat: add pagination

---
+++ APICatalog/API/DTOs/Common/PagedResponseDTO.cs
@@ -0,0 +1,26 @@
+﻿using APICatalog.Core.Common.Pagination;
+
+namespace APICatalog.API.DTOs.Common
+{
+    public class PagedResponseDTO<T> where T : class
+    {
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public bool HasPrevious { get; set; }
+        public bool HasNext { get; set; }
+        public IEnumerable<T> Items { get; set; }
+
+        public PagedResponseDTO(PagedList<T> pagedList)
+        {
+            TotalCount = pagedList.TotalCount;
+            TotalPages = pagedList.TotalPages;
+            PageSize = pagedList.PageSize;
+            CurrentPage = pagedList.CurrentPage;
+            HasPrevious = pagedList.HasPrevious;
+            HasNext = pagedList.HasNext;
+            Items = pagedList;
+        }
+    }
+}
---
+++ APICatalog/Core/Common/Pagination/PagedList.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Core.Common.Pagination
+{
+    public class PagedList<T> : List<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PagedList(List<T> items, int totalCount, int currentPage, int pageSize)
+        {
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> ToPagedList(IQueryable<T> source, int currentPage, int pageSize)
+        {
+            var totalCount =  await source.CountAsync();
+            var items = await source
+                .Skip((currentPage - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return new PagedList<T>(items, totalCount, currentPage, pageSize);
+        }
+    }
+}
