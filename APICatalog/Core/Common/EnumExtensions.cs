4c6ee7c832dc689af9adbf5295f1be3e3f58ddf8
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-05-24 14:05:26 -0300

    feat: adicionando migrations e ajuste no getDisplayName do GetOrCreateUserSystemAsync

--- APICatalog/Core/Common/EnumExtensions.cs
+++ APICatalog/Core/Common/EnumExtensions.cs
@@ -11,7 +11,16 @@ namespace APICatalog.Core.Common
             if (member == null) return enumValue.ToString();
 
             var attribute = member.GetCustomAttribute<DisplayAttribute>();
-            return attribute?.GetName() ?? enumValue.ToString();
+            if (attribute == null) return enumValue.ToString();
+
+            if (attribute.ResourceType != null)
+            {
+                var property = attribute.ResourceType.GetProperty(attribute.Name, BindingFlags.Static | BindingFlags.Public);
+                return property?.GetValue(null)?.ToString() ?? enumValue.ToString();
+            }
+
+            return attribute.Name ?? enumValue.ToString();
         }
+
     }
 }
