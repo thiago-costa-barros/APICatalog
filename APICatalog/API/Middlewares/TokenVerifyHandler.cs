d783d420e03edb246aac0b8615ebb74c28f9f0f4
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-06-10 14:14:09 -0300

    feat: adicionando middlewares e extensions para validação de jwt

---
+++ APICatalog/API/Middlewares/TokenVerifyHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace APICatalog.API.Middlewares
+{
+    public class TokenVerifyHandler
+    {
+        public static JwtBearerEvents GetEvents()
+        {
+            return new JwtBearerEvents
+            {
+                OnChallenge = async context =>
+                {
+                    context.HandleResponse(); // impede resposta padrão 401
+
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json";
+
+                    var response = new
+                    {
+                        success = false,
+                        statusCode = 401,
+                        requestTime = DateTime.UtcNow,
+                        errorType = "Unauthorized",
+                        message = "Invalid Token or Not Found."
+                    };
+
+                    var json = JsonSerializer.Serialize(response);
+                    await context.Response.WriteAsync(json);
+                }
+            };
+        }
+    }
+}
